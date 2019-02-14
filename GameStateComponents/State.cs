5bdf2300840dc1267a1087e8404dc52a9cded259
Wayne Huang <wayne.huang.21@gmail.com>
2019-02-14 00:27:49 -0800

    Added ClientManagement components - PlayerConnection object tracks a players client id, actor id, socket, and destination - ClientManager is a singleton that holds all the PlayerConnection objects - State is a singleton that holds GameState and ClientManager

---
+++ GameStateComponents/State.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GameStateComponents {
+    class State {
+        private static State instance = null;
+        private static readonly object padlock = new object();
+        private static GameState gamestate;
+        private static ClientManager clientmanager;
+
+        private State() {
+            gamestate = GameState.Instance;
+            clientmanager = ClientManager.Instance;
+        }
+
+        public static State Instance {
+            get {
+                lock (padlock) {
+                    if (instance == null) {
+                        instance = new State();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public GameState getGameState() {
+            return gamestate;
+        }
+
+        public ClientManager getClientManger() {
+            return clientmanager;
+        }
+    }
+}
